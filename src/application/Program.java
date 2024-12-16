68c91440bc7b379885dd1f0c458c062f7c4321dd
Marcela Meiser <marcela.meiser@gmail.com>
2024-12-16 17:46:44 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,7 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", null, 3000.0, obj);
 		System.out.println(seller);
-		//System.out.println(obj);
+		System.out.println(obj);
 
 	}
 
