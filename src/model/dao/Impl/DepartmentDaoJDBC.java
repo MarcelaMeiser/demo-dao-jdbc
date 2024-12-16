0e08118e7efed39bd1e374554111954b6266ed04
Marcela Meiser <marcela.meiser@gmail.com>
2024-12-16 18:39:33 -0300

    FindById Implementation

--- src/model/dao/Impl/DepartmentDaoJDBC.java
+++
@@ -1,5 +0,0 @@
-package model.dao.Impl;
-
-public class DepartmentDaoJDBC {
-
-}
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -95,6 +95,6 @@ public class Seller implements Serializable{
 	@Override
 	public String toString() {
 		return "Seller Information:\nId = " + id + "\nName = " + name + "\nEmail = " + email + "\nBirth Date = " + birthDate + "\nBase Salary = "
-				+ baseSalary + "\nDepartment = " + department;
+				+ baseSalary + "\n" + department;
 	}
 }
