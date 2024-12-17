557ff51fd08773d6edd504ce30e4a8599e5fa932
Marcela Meiser <marcela.meiser@gmail.com>
2024-12-17 13:34:36 -0300

    Delete Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Date birthDate = new Date();
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -31,17 +33,24 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		/*
 		System.out.println("\n=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", birthDate, 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new Seller, Id = " + newSeller.getId());
-		
+		*/
 		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update Completed!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Complete!");
+		
 	}
 
 }
