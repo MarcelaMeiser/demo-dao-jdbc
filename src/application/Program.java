5f37a4ec3fb6e280b3615e7bd854fc14749123c8
Marcela Meiser <marcela.meiser@gmail.com>
2024-12-17 13:27:58 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", birthDate, 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new Seller, Id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
 
 }
